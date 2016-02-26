0c7303988af2803712caab32825eba4202d9bab7
DD4444 <remi.hauperpinbonneau@live.fr>
2016-02-26 14:16:08 +0100

    architecture set

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pokesirApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pokesirApp
+ */
+angular.module('pokesirApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pokesirApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pokesirApp
+ */
+angular.module('pokesirApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
