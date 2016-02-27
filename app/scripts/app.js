089819ad2c426ed369b915626c829716fc83b514
DD4444 <remi.hauperpinbonneau@live.fr>
2016-02-27 13:06:08 +0100

    select pokemon, integrated filter using angular-ui-select

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.select'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -14,6 +14,8 @@ laxcomma:true
 */
 angular.module('pokesirApp')
 .controller('PokeSearchCtrl',function($scope, pokeFactory) {
+    $scope.pokemon = {};
+    $scope.pokemonlist = {};
     pokeFactory.pokemons.query().$promise.then(function (elem) {
         $scope.pokemonlist = _.map(elem, function(el){
             var urlsplit = el.url.split('/');
@@ -24,7 +26,6 @@ angular.module('pokesirApp')
         });
     })
     .catch(function(){
-        $scope.pokemon = [];
     })
     ;
 });
