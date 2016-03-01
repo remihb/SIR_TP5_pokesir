8da1aed9b0c733a8ef455760e1cab02843a5bb80
DD4444 <remi.hauperpinbonneau@live.fr>
2016-03-01 18:31:55 +0100

    modify background position on resize

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,15 +7,15 @@ laxcomma:true
 'use strict';
 
 /**
- * @ngdoc overview
- * @name pokesirApp
- * @description
- * # pokesirApp
- *
- * Main module of the application.
- */
+* @ngdoc overview
+* @name pokesirApp
+* @description
+* # pokesirApp
+*
+* Main module of the application.
+*/
 angular
-  .module('pokesirApp', [
+.module('pokesirApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
@@ -23,30 +23,40 @@ angular
     'ngSanitize',
     'ngTouch',
     'ui.select',
-  ])
-  .config(function ($routeProvider) {
+])
+.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
+    .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
-      })
-      .when('/about', {
+    })
+    .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
-      })
-      .when('/search', {
+    })
+    .when('/search', {
         templateUrl: 'views/search.html',
         controller: 'PokeSearchCtrl',
         controllerAs: 'search'
-      })
-      .when('/pokemon/:id', {
+    })
+    .when('/pokemon/:id', {
         templateUrl: 'views/pokemon.html',
         controller: 'PokeInfosCtrl',
         controllerAs: 'pokemon'
-      })
-      .otherwise({
+    })
+    .otherwise({
         redirectTo: '/'
-      });
-  });
+    });
+});
+
+window.onresize = function(){
+    if ((window.innerHeight/window.innerWidth) > 0.62)
+    {
+        $('html').css('background-position', 'left bottom');
+    }
+    else {
+        $('html').css('background-position', 'right bottom');
+    }
+};
